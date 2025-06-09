e22b841a365b2f4336b79d0ecadd2b59502c9525
Rian Rahman Al Aziz <rianrahman8243@gmail.com>
2025-06-09 11:59:13 +0700

    3:49:23

--- src/components/commons/molecules/HeroBanner.jsx
+++ src/components/commons/molecules/HeroBanner.jsx
@@ -4,7 +4,7 @@ function HeroBanner() {
     return (
         <section className='relative h-[460px] w-full rounded-b-4xl'>
             <img src="/images/bg-1.jpg" className='h-full w-full rounded-4xl object-cover shadow-md' />
-            <div className='absolute bg-(--bg-primary-cray) h-32 w-32 rounded-full' />
+            <div className='absolute bg-(--bg-primary-cray) h-32 w-32 rounded-full -bottom-16 -translate-x-1/2 transform left-1/2' />
         </section>
     )
 }
